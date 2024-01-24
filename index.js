d1255ad67c1ecdff642bde6519e395ed87374f5a
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-01-24 11:45:14 +0545

    Implement core functionality

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const components = {
     imageField: document.querySelector('.image-slider > img'),
     previousButton: document.querySelector('.previous-button'),
     nextButton: document.querySelector('.next-button'),
-    dots: document.querySelectorAll(".dot"),
+    dotsContainer: document.querySelector(".dots"),
     images: [
         './assets/second.jpeg',
         './assets/star.jpeg',
@@ -16,25 +16,26 @@ const components = {
         './assets/space.jpeg',
     ],
     imageIndex: 0,
-    previousIndex: 0,
+    previousDot: null,
 };
 
-function displayImageFromDot(e) {
-    components.dots.forEach(elem => {
-        if (elem === e.target) {
-            elem.classList.add("focused-circle");
-        } else if (elem.classList.contains("focused-circle")) {
-            elem.classList.remove("focused-circle");
-        }
-    })
+function createDot(index) {
+    const element = document.createElement("button");
+    element.classList.add("dot");
+    element.dataset.index = index;
+    document.querySelector(".dots").appendChild(element);
 }
 
 window.onload = () => {
     if (components.imageField)
         components.imageField.src = components.images[0];
+    for (let i = 0; i < components.images.length; i++) {
+        createDot(i);
+    }
 }
 
 components.mainContainer.addEventListener('click', (e) => {
+    components.previousIndex = components.imageIndex;
     if (e.target.nodeName !== "BUTTON")
         return;
     if (e.target.classList.contains('previous-button')) {
@@ -42,13 +43,19 @@ components.mainContainer.addEventListener('click', (e) => {
     } else if (e.target.classList.contains('next-button')) {
         components.imageIndex++;
     }
-    components.previousIndex = components.imageIndex;
     components.imageIndex = (components.imageIndex + components.images.length) 
                                                    % components.images.length;
     components.imageField.src = components.images[components.imageIndex];
 })
 
-
-components.dots.forEach(dot => {
-    dot.addEventListener('click', displayImageFromDot)
+components.dotsContainer.addEventListener('click', (e) => {
+    if (e.target.classList.contains("dot")) {
+        if  (components.previousDot) {
+            components.previousDot.classList.remove("focused-circle");
+        }         
+        e.target.classList.add("focused-circle");
+        components.previousDot = e.target;
+        components.imageIndex = parseInt(e.target.dataset.index);
+        components.imageField.src = components.images[components.imageIndex];
+    }
 })
